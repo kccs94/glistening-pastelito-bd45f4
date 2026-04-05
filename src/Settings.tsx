29975a89db059e7953e5cf098897c2035b538f7a
kccs94 <cschan.work@gmail.com>
2026-04-05 20:44:07 +0800

    Add staff management UI to Settings.tsx for managing staff per outlet

---
+++ src/Settings.tsx
@@ -0,0 +1,14 @@
+// Staff Management UI Component
+import React from 'react';
+
+const StaffManagement = () => {
+    return (
+        <div>
+            <h2>Staff Management</h2>
+            {/* Implement your staff management UI here */}
+            <p>Manage staff per outlet here.</p>
+        </div>
+    );
+};
+
+export default StaffManagement;
